2420d7e9725c60338779118f0c0e82ec97c25299
Andrew Myers <afredmyers@gmail.com>
2023-05-09 11:35:08 -0400

    Refactors SonyCi configuration and more
    * Allows config to be loaded from TOML, ENV, and params.
    * Adds 'get' and 'post' wrapper methods to SonyCi that automatically convert Response
      objects to JSON.
    * Sets VCR record mode to "none" for github continuous integration.
    * Adds ci.toml to .gitignore, a common place for storing local Sony Ci credentials.
    * Adds Jupyter Notebook for CLI usage examples.
    * Adds some logging.
    * Updates VCR cassettes for testing.
    
    ---------
    
    Co-authored-by: Harpo <ryan_harbert@wgbh.org>

--- sonyci/sonyci.py
+++ sonyci/sonyci.py
@@ -1,34 +1,23 @@
 from typing import Any
 
-from pydantic import BaseModel
 from requests_oauth2client import ApiClient, OAuth2Client
-from requests_oauth2client.auth import OAuth2AccessTokenAuth
+from requests_oauth2client.auth import BearerAuth, OAuth2AccessTokenAuth
 from requests_oauth2client.tokens import BearerToken
 
-from sonyci.config import BASE_URL, TOKEN_URL
-from sonyci.utils import get_token
+from sonyci.config import Config
+from sonyci.log import log
+from sonyci.utils import get_token, json
 
 
-class SonyCi(BaseModel, extra='allow'):
-    """SonyCI API client
+class SonyCi(Config):
+    class Config:
+        arbitrary_types_allowed = True
 
-    Attributes:
-        username (str, optional): SonyCI username.
-        password (str): SonyCI password.
-        client_id (str): SonyCI client ID.
-        client_secret (str): SonyCI client secret.
-        workspace_id (str): SonyCI workspace ID.
-        token (BearerToken): SonyCI token.
+    """A SonyCI client."""
 
-    """
-
-    base_url: str = BASE_URL
-    token_url: str = TOKEN_URL
-    username: str | None = None
-    password: str | None = None
-    client_id: str | None = None
-    client_secret: str | None = None
-    workspace_id: str | None = None
+    # This will not be needed when we upgrade to pydantic2,
+    # we will be able to directly overwrite the @cached_property instance
+    t: BearerToken = None
 
     @property
     def oauth(self) -> OAuth2Client:
@@ -46,8 +35,13 @@ class SonyCi(BaseModel, extra='allow'):
     @property
     def token(self) -> BearerToken:
         """Get a token from SonyCI and cache the results."""
+        if self.t:
+            return self.t
         return get_token(
-            self.username, self.password, self.client_id, self.client_secret
+            username=self.username,
+            password=self.password,
+            client_id=self.client_id,
+            client_secret=self.client_secret,
         )
 
     @property
@@ -56,7 +50,9 @@ class SonyCi(BaseModel, extra='allow'):
 
         This will refresh the token automatically if it is expired.
         """
-        return OAuth2AccessTokenAuth(client=self.oauth, token=self.token)
+        if self.client_id and self.client_secret:
+            return OAuth2AccessTokenAuth(client=self.oauth, token=self.token)
+        return BearerAuth(token=self.token)
 
     @property
     def client(self) -> ApiClient:
@@ -70,10 +66,23 @@ class SonyCi(BaseModel, extra='allow'):
         return ApiClient(self.base_url, auth=self.auth)
 
     @property
-    def workspace(self) -> str:
-        """Return the workspace prefix for API calls."""
-        return f'workspaces/{self.workspace_id}'
+    def workspaces(self):
+        return self.get('workspaces')['items']
+
+    @property
+    def workspace(self):
+        """Return response of /workspaces/{workspace_id}"""
+        return self.get(f'workspaces/{self.workspace_id}')
+
+    @json
+    def get(self, *args, **kwargs):
+        log.debug(f'GET {args} {kwargs}')
+        return self.client.get(*args, **kwargs)
+
+    @json
+    def post(self, *args, **kwargs):
+        return self.client.post(*args, **kwargs)
 
     def __call__(self, path: str, **kwds: Any) -> Any:
         """Default action: make GET request to Sony CI."""
-        return self.client.get(path, **kwds)
+        return self.get(path, **kwds)
--- tests/test_sonyci.py
+++ tests/test_sonyci.py
@@ -1,28 +1,39 @@
+from os import environ
+
 from pytest import fixture, mark
 
 from sonyci import SonyCi
+from sonyci.config import Config
 
 
 @fixture(scope='module')
-def ci():
-    return SonyCi(
-        username='username',
-        password='password',
-        client_id='client_id',
-        client_secret='client_secret',
-        workspace_id='f44132c2393d470c88b9884f45877ebb',
-    )
+def ci_config():
+    if environ.get('RECORD'):
+        return Config.load('./ci.toml')
+    return Config.load('./tests/sonyci/sonyci.toml')
+
+
+@fixture(scope='module')
+def ci(ci_config: Config):
+    return SonyCi(**ci_config.dict())
 
 
 @mark.vcr()
-def test_token(ci):
+def test_token(ci: SonyCi):
     token = ci.token
     assert type(token.access_token) is str, 'Access token is not a string'
     assert len(token.access_token) > 0, 'Access token was empty'
 
 
 @mark.vcr()
-def test_workspace(ci):
-    response = ci.client.get(f'workspaces/{ci.workspace_id}')
-    assert response.status_code == 200, 'Workspace did not return 200'
-    assert response.json()['id'] == ci.workspace_id, 'Workspace id did not match'
+def test_workspaces(ci: SonyCi):
+    workspaces = ci.workspaces
+    assert type(workspaces) == list, 'workspaces is not a list'
+    assert len(workspaces) > 0, 'no workspaces found'
+
+
+@mark.vcr()
+def test_workspace(ci: SonyCi):
+    workspace = ci.workspace
+    assert type(workspace) == dict, 'workspace is not a dict'
+    assert 'id' in workspace, 'workspace has no id'
