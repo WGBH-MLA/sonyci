1fe8d119b3a509ade5622ebe3c896fa11ebd9277
Andrew Myers <afredmyers@gmail.com>
2023-05-10 12:52:02 -0400

    🍑 Assets (#15)
    * Adds function for Sony Ci endpoint /assets/[asset_id]
    
    * ruff linting

--- sonyci/sonyci.py
+++ sonyci/sonyci.py
@@ -89,6 +89,9 @@ class SonyCi(Config):
             'items'
         ]
 
+    def asset(self, asset_id: str = None, **kwargs):
+        return self.get(f'/assets/{asset_id}', params=kwargs)
+
     @json
     def get(self, *args, **kwargs):
         log.debug(f'GET {args} {kwargs}')
