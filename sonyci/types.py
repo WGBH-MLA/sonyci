0e1119809435d278b52869f4b28cf02cf4bd39f9
Harpo Harbert <ryan_harbert@wgbh.org>
2023-05-22 14:26:13 -0700

    Fixes possible ProxyTypes

--- sonyci/types.py
+++ sonyci/types.py
@@ -3,8 +3,5 @@ from enum import Enum
 
 class ProxyType(Enum):
     document_pdf = 'document-pdf'
-    small = 'small'
-    medium = 'medium'
-    large = 'large'
-    standard = 'standard'
-    px_2000 = '2000px'
+    video_sd = 'video-sd'
+    video_3g = 'video-3g'
